8053f4d86e417963d2907d0ec95f28a550926274
oplindst <olli-pekka.lindstrom@helsinki.fi>
2014-04-06 22:36:53 +0300

    4vko loppu

--- libs/models/pokemonlaji.php
+++ libs/models/pokemonlaji.php
@@ -60,6 +60,30 @@ class Pokemonlaji {
         }
         return $tulokset[0];
     }
+    
+    public static function etsiPokemoneja($id, $nimi, $type1, $type2, $hp, $atk, $def, $spatk, $spdef, $spd) {
+        require_once "libs/tietokantayhteys.php";
+        $sql = "Select * from Pokemonlaji where ID = ?, Nimi = ?, Type1 = ?, Type2 = ?, BHP = ?, BAtk = ?, BDef = ?, BSpAtk = ?, BSpDef = ?, BSpd = ? WHERE ID = ?";
+        $kysely = Yhteys::getTietokantayhteys()->prepare($sql);
+        $kysely->execute(array($this->ID, $this->Nimi, $this->Type1, $this->Type2, $this->BHP, $this->BAtk, $this->BDef, $this->BSpAtk, $this->BSpDef, $this->BSpd));
+        
+        $tulokset = array();
+        foreach ($kysely->fetchAll(PDO::FETCH_OBJ) as $tulos) {
+            $laji = new Pokemonlaji();
+            $laji->setId($tulos->id);
+            $laji->setName($tulos->nimi);
+            $laji->setType1($tulos->type1);
+            $laji->setType2($tulos->type2);
+            $laji->setHP($tulos->bhp);
+            $laji->setAtk($tulos->batk);
+            $laji->setDef($tulos->bdef);
+            $laji->setSpAtk($tulos->bspatk);
+            $laji->setSpDef($tulos->bspdef);
+            $laji->setSpd($tulos->bspd);
+            $tulokset[] = $laji;
+        }
+        return $tulokset;
+    }
 
     public static function etsiKaikkiPokemonit() {
 
